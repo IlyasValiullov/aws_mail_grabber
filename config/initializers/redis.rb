95a2c1bb9c5f8ac2ed4c4bb9fffe39f55d3f579b
Ilyas Valiullov <ivaliullov@b-factory.ru>
2017-09-17 18:24:46 +0300

    change redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,2 @@
-$redis = Redis::Namespace.new("ihms_env_app", :redis => Redis.new)
-
 uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
